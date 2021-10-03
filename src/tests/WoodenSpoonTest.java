c7c0f433f083f5a9b1a8b179a9810a6deb8027d0
josue <josue.garcia44@yahoo.com>
2021-10-03 16:29:27 -0400

    added woodenspoon test

---
+++ src/tests/WoodenSpoonTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class WoodenSpoonTest {
+
+    public static void main(String[] args) {
+      for (int i = 0; i < 5; i++){
+          System.out.println("wooden Spoon");
+      }
+    }
+
+
+
+
+
+}
